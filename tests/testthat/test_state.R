24162207dec89ee07b87d506848632c176cc7bc9
Tom Elliott <tomelliottnz@gmail.com>
2022-01-05 11:12:21 +1300

    add test

--- tests/testthat/test_state.R
+++ tests/testthat/test_state.R
@@ -4,3 +4,9 @@ test_that("Initializes correctly", {
     expect_s3_class(s$docs, "inzdocs")
     expect_s3_class(s$controls, "inzcontrols")
 })
+
+test_that("Data can be loaded", {
+    s <- inzight()
+    a <- inzaction("LOAD_DATA")
+    # dispatch()
+})
